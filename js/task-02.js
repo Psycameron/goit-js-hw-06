e63c40aa3ef3a4193f4332305a474d125b83bf2d
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-18 23:58:10 +0200

    Создал 1 элемент во 2 задаче

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const item = document.createElement("li");
+item.textContent = ingredients[0];
+console.log(`🚀 ~ item`, item);
