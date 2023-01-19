4d9b384c59a28ce2f43c393e2540445b5af2fbb9
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-19 20:21:54 +0200

    Сделал 3 задачу

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,27 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeup = images
+  .map(
+    (image) =>
+      `<li><img class = gallery--image, src="${image.url}" alt="${image.alt}", width = 1000px ></li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", makeup);
+
+// 5 - сделать оформление в css
