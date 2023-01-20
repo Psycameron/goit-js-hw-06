b21d9f835daa03b11c780c85e3929ee74a1922bf
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-20 17:57:20 +0200

    Сделал 8 задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("Заполните пустые поля");
+  }
+  event.preventDefault();
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(`Данные пользователя`, formData);
+  form.reset();
+}
