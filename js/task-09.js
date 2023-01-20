10eff7d0b1a3b66a7652c44bf56d0a57b07f3de0
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-20 22:02:01 +0200

    Сделал 9 задачу

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  span.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+  return;
 }
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", getRandomHexColor);
